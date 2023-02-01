152564a1d400ad8d0cc87ba4a24f9d943bd9ab74
Ivan <iuljevic@dsgroup.hr>
2023-02-01 09:26:51 +0100

    Add project files.

---
+++ NZwalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZwalks.API.Model.Domain;
+
+namespace NZwalks.API.Controllers.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalksDifficulty { get; set; }
+
+    }
+}
